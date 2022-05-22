f07675fefcab66918a1db2900ed8a0fc14c49ff4
Sebi <stoicescusebastian@yahoo.com>
2022-05-22 10:59:39 +0300

    [BE] add isAuth middleware

--- src/routes/crypto/crypto.js
+++ src/routes/crypto/crypto.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const server = express()
 const Currency = require('../../models/currency')
+const isAuth = require('../../middleware/isAuth')
 
-server.get('/crypto', async (req, res) => {
+server.get('/crypto', isAuth, async (req, res) => {
     const availableCrypto = await Currency.find();
   
     if (!availableCrypto) {
