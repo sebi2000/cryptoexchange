ea37a9ff809ac4090e88eae7a755f4d6cd644632
andreihojda15 <andrei.hojda00@gmail.com>
2022-05-25 19:23:50 +0300

    [BE] crypto: endpoint for crypto to sell

--- src/routes/crypto/crypto.js
+++ src/routes/crypto/crypto.js
@@ -2,19 +2,43 @@ const express = require('express')
 const server = express()
 const Currency = require('../../models/currency')
 const isAuth = require('../../middleware/isAuth')
+const Wallet = require('../../models/wallet')
 
 server.get('/crypto', isAuth, async (req, res) => {
-    const availableCrypto = await Currency.find();
+  const availableCrypto = await Currency.find();
+
+  if (!availableCrypto) {
+    res.status(404).json({
+      message: "No crypto available",
+    })
+  } else {
+    res.status(200).json({
+      availableCrypto,
+    })
+  }
+})
+
+server.get('/crypto-sell', isAuth, async (req, res) => {
+  const user = req.session.passport.user;
+  const wallet = await Wallet.findOne({ userId: user._id });
   
-    if (!availableCrypto) {
-      res.status(404).json({
-        message: "No crypto available",
-      })
-    } else {
-      res.status(200).json({
-        availableCrypto,
-      })
+  if (!wallet) {
+    return res.status(404).json({
+      msg: 'Wallet not found'
+    });
+  }
+
+  const toSell = []
+  for (const c of wallet.currency) {
+    const currency = await Currency.findById(c.currencyId);
+    if (currency.name !== 'xUSD') {
+      toSell.push({ name: currency.name, amount: c.amount, price: currency.ratio * c.amount })
     }
+  }
+  
+  return res.status(200).json(
+    toSell
+    )
   })
 
-module.exports = server
+  module.exports = server
