3f9b020c02b447313a83b4cba2ceb784fcf00262
andreihojda15 <andrei.hojda00@gmail.com>
2022-05-25 11:23:43 +0300

    [BE] wallet: implemented suggestions

--- src/routes/crypto/wallet.js
+++ src/routes/crypto/wallet.js
@@ -7,14 +7,13 @@ server.get('/wallet', isAuth, async (req, res) => {
     const wallet = await Wallet.find({ userId: req.session.passport.user._id });
 
     if (!wallet) {
-        res.status(404).json({
+        return res.status(404).json({
             msg: "No wallet found"
         })
-    } else {
-        res.status(200).json({
-            wallet
-        })
     }
+    return res.status(200).json({
+        wallet
+    })
 })
 
 module.exports = server
